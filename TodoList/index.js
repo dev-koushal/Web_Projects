195f313d4e9a05ba96117760a4afa78ad05c20ec
dev-koushal <koushalydv99@gmail.com>
2025-05-04 11:24:20 +0530

    new

--- TodoList/index.js
+++ TodoList/index.js
@@ -1,16 +1,17 @@
 //  blocking inspect
-document.oncontextmenu = () =>{
-    alert("Do not try to right click")
-    return false;
-}
+// document.oncontextmenu = () =>{
+//     alert("Do not try to right click")
+//     return false;
+// }
 
-document.onkeydown = e=>{
-    if (e.key == "F12"){
-        alert("Don't try to inspect element")
-        return false;
-    }
-    if(e.ctrlKey && e.key =="u"){
-        alert("Don't try to copy page element")
-        return false;
-    }
-}
+// document.onkeydown = e=>{
+//     if (e.key == "F12"){
+//         alert("Don't try to inspect element")
+//         return false;
+//     }
+//     if(e.ctrlKey && e.key =="u"){
+//         alert("Don't try to copy page element")
+//         return false;
+//     }
+// }
+
