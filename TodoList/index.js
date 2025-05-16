9d8f2d1918900e9f35cd8617ebb3f6d17e653f70
dev-koushal <koushalydv99@gmail.com>
2025-05-16 11:00:20 +0530

    to_do

--- TodoList/index.js
+++ TodoList/index.js
@@ -1,25 +1,41 @@
-//  blocking inspect
-// document.oncontextmenu = () =>{
-//     alert("Do not try to right click")
-//     return false;
-// }
-
-// document.onkeydown = e=>{
-//     if (e.key == "F12"){
-//         alert("Don't try to inspect element")
-//         return false;
-//     }
-//     if(e.ctrlKey && e.key =="u"){
-//         alert("Don't try to copy page element")
-//         return false;
-//     }
-// }
-
 const checkBoxList = document.querySelectorAll('.custom-checkbox')
 const inputFields = document.querySelectorAll('.goal-input')
 const errorLabel = document.querySelector('.error-label')
+const progressLabel = document.querySelector('.progress-label')
 const progressBar = document.querySelector('.progress-bar')
 const progressValue = document.querySelector('.progress-value')
+
+const allQuotes = [
+  'Raise the bar by completing your goals!',
+  'Well begun is half done!',
+  'Just a step away, keep going!',
+  'Whoa! You just completed all the goals, time for chill :D',
+]
+
+// const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {
+//   first: {
+//     name: '',
+//     completed: false,
+//   },
+//   second: {
+//     name: '',
+//     completed: false,
+//   },
+//   third: {
+//     name: '',
+//     completed: false,
+//   },
+// }
+
+const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
+
+let completedGoalsCount = Object.values(allGoals).filter(
+  (goal) => goal.completed
+).length
+
+progressValue.style.width = `${(completedGoalsCount / inputFields.length) * 100}%`
+progressValue.firstElementChild.innerText = `${completedGoalsCount}/${inputFields.length} completed`
+progressLabel.innerText = allQuotes[completedGoalsCount]
 
 checkBoxList.forEach((checkbox) => {
   checkbox.addEventListener('click', (e) => {
@@ -29,17 +45,51 @@ checkBoxList.forEach((checkbox) => {
 
     if (allGoalsAdded) {
       checkbox.parentElement.classList.toggle('completed')
-      progressValue.style.width = '33.33%'
+      const inputId = checkbox.nextElementSibling.id
+      allGoals[inputId].completed = !allGoals[inputId].completed
+      completedGoalsCount = Object.values(allGoals).filter(
+        (goal) => goal.completed
+      ).length
+
+      progressValue.style.width = `${(completedGoalsCount / inputFields.length) * 100}%`
+      progressValue.firstElementChild.innerText = `${completedGoalsCount}/${inputFields.length} completed`
+      progressLabel.innerText = allQuotes[completedGoalsCount]
+
+      localStorage.setItem('allGoals', JSON.stringify(allGoals))
     } else {
-        progressBar.classList.add('show-error')
+      progressBar.classList.add('show-error')
     }
   })
 })
 
 inputFields.forEach((input) => {
-    input.addEventListener('focus', () => {
-        progressBar.classList.remove('show-error')
-    })
-})
+  if (allGoals[input.id]) {
+    input.value = allGoals[input.id].name
 
-// ewe need to see each field to checkup the final green tick we need ot set it intioal= 
+    if (allGoals[input.id].completed) {
+      input.parentElement.classList.add('completed')
+    }
+  }
+
+  input.addEventListener('focus', () => {
+    progressBar.classList.remove('show-error')
+  })
+
+  input.addEventListener('input', (e) => {
+    if (allGoals[input.id] && allGoals[input.id].completed) {
+      input.value = allGoals[input.id].name
+      return
+    }
+
+    if (allGoals[input.id]) {
+      allGoals[input.id].name = input.value
+    } else {
+      allGoals[input.id] = {
+        name: input.value,
+        completed: false,
+      }
+    }
+
+    localStorage.setItem('allGoals', JSON.stringify(allGoals))
+  })
+})
